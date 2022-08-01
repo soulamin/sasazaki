1101ab0931fbb41f2d961dfd79417324acf0e146
Alan Lamin <lamin.exe@gmail.com>
2022-08-01 20:09:07 -0300

    api contagem material

--- api/contmaterial_api/index.php
+++ api/contmaterial_api/index.php
@@ -8,13 +8,18 @@
  */
 require '../../fontes/conexao.php';
 
+$codmodelo       = isset($_GET['mod'])? ' AND p.codigo_modelo = "'.$_GET['mod'].'"':"";
+$codambiente     = " pa.codigo_ambiente = '".$_GET['amb']."'";
 
 
- $stmt = $pdo->prepare("SELECT COUNT(m.id) as quantidade , m.codigo_material ,
+$stmt = $pdo->prepare("SELECT COUNT(m.id) as quantidade , m.codigo_material ,
                                                                 m.nome_material FROM materiais m 
                                                                INNER JOIN produtos p ON p.codigo_material = m.codigo_material 
+                                                               INNER JOIN produtos_ambientes pa ON p.codigo_produto =  pa.codigo_produto
+                                                               INNER JOIN ambientes a ON a.codigo_ambiente =  pa.codigo_ambiente
+                                                               WHERE ".$codambiente.$codmodelo."
                                                                GROUP BY m.codigo_material");
-
+var_dump($stmt);
 $executa = $stmt->execute();
 $material = array();
 
