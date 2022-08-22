4027f246508431cee3b32f4de3bebf06e31c4008
Marcos Ramos <marcos.ramos@beeid.com.br>
2022-08-22 08:49:14 -0300

    inserção acessorios api

--- api/produtosopcionais_api/index.php
+++ api/produtosopcionais_api/index.php
@@ -2,41 +2,48 @@
 
 require '../../fontes/conexao.php';
 
-$itempai             = isset($_GET['itempai'])   ? ' p.item_pai = "'.$_GET['itempai'].'"' : "";
-$codproduto          = isset($_GET['codproduto'])? ' codigo_produto = "'.$_GET['codproduto'].'"':"";
+$codproduto = isset($_GET['codproduto'])? ' codigo_produto = "'.$_GET['codproduto'].'"':"";
 
-$stmt_opc = $pdo->prepare("SELECT codigo_produto, codigo_opcional FROM produtos_opcionais
-                           WHERE ".$codproduto);
+$stmt_opc = $pdo->prepare("SELECT codigo_opcional FROM produtos_opcionais WHERE ".$codproduto);
 
 $executa = $stmt_opc->execute();
-
 
 $produtos = array();
 
 
 while ($linha = $stmt_opc->fetch(PDO::FETCH_ASSOC)) {
-    
-    $stmt = $pdo->prepare("SELECT p.*,l.nome_linha, v.tipo_vidro , o.nome_opcional ,m.nome_modelo,i.nome_material,
-                         d.descricao_diferencial ,c.nome_cor FROM produtos p
-                         INNER JOIN produtos_vidros pv ON pv.codigo_produto=p.codigo_produto
-                         INNER JOIN produtos_opcionais po ON po.codigo_produto=p.codigo_produto
-                         INNER JOIN produtos_diferenciais pd ON pd.codigo_produto=p.codigo_produto
-                         INNER JOIN vidros v ON pv.codigo_vidro=v.codigo_vidro
-                         INNER JOIN linhas l ON l.codigo_linha=p.id_linha
-                         INNER JOIN modelos m ON m.codigo_modelo=p.codigo_modelo
-                         INNER JOIN materiais i ON i.codigo_material=p.codigo_material
-                         INNER JOIN cores c ON c.codigo_cor=p.codigo_cor
-                         INNER JOIN opcionais o ON po.codigo_opcional=o.codigo_opcional
-                         INNER JOIN diferenciais d ON pd.codigo_diferencial=d.codigo_diferencial
-                         WHERE ".$itempai);
-     
-     $stmt->bindParam(':codigo_produto', $linha['codigo_produto']);
-     $stmt->execute();         
+
+    //print_r($linha);
+    //print_r($linha['codigo_opcional']);
+
+    $stmt_opc_pai = $pdo->prepare("SELECT p.it_codigo from produtos p WHERE p.codigo_produto ='".$linha['codigo_opcional']."'"); 
+
+    $executa = $stmt_opc_pai->execute();
+
+    while ($linha_opc = $stmt_opc_pai->fetch(PDO::FETCH_ASSOC)) {
+        
+        $stmt = $pdo->prepare("SELECT p.*,l.nome_linha, m.nome_modelo,i.nome_material,c.nome_cor 
+        FROM produtos p
+        INNER JOIN linhas l ON l.codigo_linha=p.id_linha
+        INNER JOIN modelos m ON m.codigo_modelo=p.codigo_modelo
+        INNER JOIN materiais i ON i.codigo_material=p.codigo_material
+        INNER JOIN cores c ON c.codigo_cor=p.codigo_cor
+        WHERE p.it_codigo ='".$linha_opc['it_codigo']."'");
+
+        $executa = $stmt->execute();
+
+        while($linha_final = $stmt->fetch(PDO::FETCH_ASSOC))
+        {                       
+            array_push($produtos, $linha_final);
+        }
        
+        //$stmt->bindParam(':it_codigo', $linha_opc['it_codigo']);
+        //$stmt->execute();       
+      
+        }
+      
     }
-    array_push($produtos, $linha);
-  
-}
+     
 
 $Resultado['produtos'] = $produtos;
 echo json_encode($Resultado);
